0de0203d8bce76ba0fa12ee3085d2b291612dbac
JuanraCM <juanracm.dev@gmail.com>
2025-03-02 14:14:23 +0000

    feat: add PWA support

--- src/app.ts
+++ src/app.ts
@@ -90,3 +90,14 @@ Alpine.data('active', () => ({
 }));
 
 Alpine.start();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./sw.js')
+    .then(() => {
+      console.log('Service Worker Registered');
+    })
+    .catch((error) => {
+      console.error('Service Worker Registration Failed:', error);
+    });
+}
